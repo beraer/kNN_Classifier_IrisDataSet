6971e0d8a4a21adc38d158570cab2992f7e70871
beraer <s29315@pjwstk.edu.pl>
2025-03-13 22:17:53 +0100

    DataPoint class was added. DataPoint holds a feature vector and its corresponding label

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
+    private static class DataPoint{
+        double[] features;
+        String label;
+
+        public DataPoint(double[] features, String label){
+            this.features = features;
+            this.label = label;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
