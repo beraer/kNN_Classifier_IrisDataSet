fd8b2a2562d7e0a305cc45f6cecf310f390cb0c9
beraer <s29315@pjwstk.edu.pl>
2025-03-13 22:24:25 +0100

    loadDataFile method was added. this method can read all csv type files as long as label is the last toen of the line.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     private static class DataPoint{
@@ -8,6 +14,22 @@ public class Main {
             this.features = features;
             this.label = label;
         }
+    }
+
+    private static List<DataPoint> loadDataFile(String filename) throws IOException {
+        List<DataPoint> dataPoints = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line;
+        while((line = br.readLine()) != null){
+            String[] tokens = line.split(",");
+            double[] features = new double[tokens.length - 1];
+            for(int i = 0; i < features.length; i++){
+                features[i] = Double.parseDouble(tokens[i]);
+            }
+            String label = tokens[tokens.length - 1];
+            dataPoints.add(new DataPoint(features, label));
+        }
+        return dataPoints;
     }
 
     public static void main(String[] args) {
